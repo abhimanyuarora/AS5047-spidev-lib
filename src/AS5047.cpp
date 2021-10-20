50ff0f31b60ba457e6143626edf3ec39343a49bf
Abhimanyu <abhi.arora.sg@gmail.com>
2021-10-20 11:45:48 +0000

    Initial commit

---
+++ src/AS5047.cpp
@@ -0,0 +1,133 @@
+#include <AS5047.h>
+
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/spi/spidev.h>
+
+#include <cstdio>
+
+AS5047::AS5047(const char* p_spidev, uint32_t speed) {
+    m_spidev = NULL;
+    if (p_spidev != NULL ) {
+        m_spidev = (char *) malloc(strlen(p_spidev)+1);
+        if (m_spidev != NULL) 
+            strcpy(m_spidev,p_spidev);
+    }
+    speed_ = speed;
+    m_open = false;
+}
+
+AS5047::~AS5047() {
+    if (m_spidev != NULL ) {
+        free(m_spidev);
+        m_spidev = NULL;
+    }
+    if (m_open)
+        close(m_spifd);
+}
+
+bool AS5047::begin() {
+    if (m_open == true )
+       return true;
+    if (m_spidev == NULL)
+       return false;
+
+    m_spifd = open(m_spidev, O_RDWR);
+    if (m_spifd < 0) {
+        return false;
+    }
+
+    uint8_t mode = SPI_MODE_1;
+    uint8_t bits_per_word = 8;
+
+    /* Set SPI_POL and SPI_PHA */
+    if (ioctl(m_spifd, SPI_IOC_WR_MODE, &mode) < 0) {
+        close(m_spifd);
+        return false;
+    }
+
+    /* Set bits per word*/
+    if (ioctl(m_spifd, SPI_IOC_WR_BITS_PER_WORD, &bits_per_word) < 0) {
+        close(m_spifd);
+        return false;
+    }
+
+    /* Set SPI speed*/
+    if (ioctl(m_spifd, SPI_IOC_WR_MAX_SPEED_HZ, &speed_) < 0) {
+        close(m_spifd);
+        return false;
+    }
+
+    m_open = true;
+
+    return true;
+}
+
+uint16_t AS5047::readAngle() {
+    struct spi_ioc_transfer spi_message_tx;
+    memset(&spi_message_tx, 0, sizeof(spi_message_tx));
+    spi_message_tx.tx_buf = (unsigned long) commands.readAngle;
+    spi_message_tx.len = 2;
+    if (!ioctl(m_spifd, SPI_IOC_MESSAGE(1), spi_message_tx))
+        return -1;
+
+    struct spi_ioc_transfer spi_message_rx;
+    memset(&spi_message_rx, 0, sizeof(spi_message_rx));
+    uint8_t rxbuf[2];
+    spi_message_rx.rx_buf = (unsigned long) rxbuf;
+    spi_message_rx.len = 2;
+    if (!ioctl(m_spifd, SPI_IOC_MESSAGE(1), spi_message_rx))
+        return -1;
+
+    uint16_t resp = (rxbuf[1]) | (rxbuf[0] << 8);
+
+    if (!readSanityCheck(resp)) return -1;
+
+    return resp & 0x3FFF; // Data is present in first 14 bits.
+}
+
+uint16_t AS5047::getRelAngle() {
+    int32_t temp = readAngle()-zeroOffset;
+    if (temp < 0) return 16383 + temp;
+    else return temp;
+}
+
+void AS5047::softZero() {
+    uint32_t sum = 0;
+    uint16_t last = 0;
+    uint16_t curr = 0;
+    for (int i = 0; i < numSamples_zero; i++) {
+        curr = readAngle();
+        if (i > 0) {
+            if (curr - last > 10) {
+                i--;
+                continue;
+            }
+        }
+        last = curr;
+        sum += curr;
+    }
+    zeroOffset = sum/numSamples_zero;
+}
+
+uint8_t AS5047::calcEvenParity(uint16_t msg) {
+    int parity = 0;
+    while (msg) {
+        parity ^= msg & 0b1;
+        msg >>= 1;
+    }
+    return parity;
+}
+
+bool AS5047::readSanityCheck(uint16_t msg) {
+    int recParity = (msg >> 15);
+    if (calcEvenParity(msg&0x8000) != recParity) return false;
+
+    if ((msg >> 14) & 1) return false;
+
+    return true;
+}
---
+++ src/AS5047.h
@@ -0,0 +1,37 @@
+#ifndef AS5047_H
+#define AS5047_H
+
+#include <stdint.h>
+
+#define ANGLEUNC_ADDR 0x3FFE
+
+class AS5047 {
+    private:
+        char* m_spidev;
+        int m_spifd;
+        bool m_open;
+
+        uint32_t speed_;
+
+        const uint8_t numSamples_zero = 10;
+        uint16_t zeroOffset = 0;
+
+        const struct {
+            uint8_t readAngle[2] = {0x7F, 0xFE};
+        } commands;
+
+        uint8_t calcEvenParity(uint16_t msg);
+        bool readSanityCheck(uint16_t msg);
+
+    public:
+        AS5047(const char* p_spidev, uint32_t speed=100000);
+        ~AS5047();
+
+        bool begin();
+
+        uint16_t readAngle();
+        uint16_t getRelAngle();
+        void softZero();
+};
+
+#endif
